1256201db6958fb0abf1fb533ec80d3181bcd6d7
jpernesz <1020894@student.sae.edu.au>
2020-10-06 02:55:22 +1100

    script for moving red car added

---
+++ Assets/Scripts/RedCarMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCarMover : MonoBehaviour
+{
+    public Vector3 targetPosition; // reference to our target position
+    public Transform startingPos; // The transform postion we start from
+    public Transform endPos; // The transform position we are ending at
+    public float minDistanceToTarget = 1f; // need to check this with Nathan but I think it's the distance where the Gaurd Capsule says its too close to the A or B transform position
+    public float speed = 2f; // the speed the gaurd capsule moves at 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = startingPos.position;
+        transform.position = targetPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+
+        if (Vector3.Distance(transform.position, targetPosition) <= minDistanceToTarget)
+        {
+            if (targetPosition == startingPos.position)
+            {
+                targetPosition = endPos.position;
+            }
+            else if (targetPosition == endPos.position)
+            {
+                targetPosition = startingPos.position;
+            }
+        }
+    }
+}
